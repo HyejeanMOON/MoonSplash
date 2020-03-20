6d247a2402f7922b57c7f9970b66914234328a26
HyejeanMoon <wenhaichen1992@gmail.com>
2020-03-20 16:53:15 +0900

    create photos' remote data source

--- app/src/main/java/com/hyejeanmoon/wallpaperbyunsplash/DI/ApplicationModule.kt
+++ app/src/main/java/com/hyejeanmoon/wallpaperbyunsplash/DI/ApplicationModule.kt
@@ -4,7 +4,11 @@ import android.app.Application
 import android.content.Context
 import com.hyejeanmoon.wallpaperbyunsplash.data.api.retrofit.ApiClientHelper
 import com.hyejeanmoon.wallpaperbyunsplash.data.api.retrofit.OkHttpClientSingleton
+import com.hyejeanmoon.wallpaperbyunsplash.data.scenes.photos.PhotoRemoteDataSourceImpl
 import com.hyejeanmoon.wallpaperbyunsplash.data.scenes.photos.api.service.PhotosApiService
+import com.hyejeanmoon.wallpaperbyunsplash.domain.scenes.photos.PhotosModel
+import com.hyejeanmoon.wallpaperbyunsplash.domain.scenes.photos.datasource.PhotoRemoteDataSource
+import com.hyejeanmoon.wallpaperbyunsplash.domain.scenes.photos.model.PhotosModelImpl
 import com.hyejeanmoon.wallpaperbyunsplash.utils.EnvParameters
 import dagger.Module
 import dagger.Provides
@@ -14,6 +18,7 @@ import javax.inject.Singleton
  * ApiModule
  *
  * It's a Module of Dagger.
+ * ApplicationModule will provide a instance of class automatically by dagger.
  */
 @Module
 class ApplicationModule {
@@ -33,19 +38,34 @@ class ApplicationModule {
 
     @Provides
     @Singleton
-    internal fun provideOkhttpSingleton(): OkHttpClientSingleton {
+    internal fun provideOkHttpSingleton(): OkHttpClientSingleton {
         return OkHttpClientSingleton()
     }
 
     @Provides
     @Singleton
     internal fun providePhotosApiService(
-        apiClientHelper: ApiClientHelper,
-        okHttpClientSingleton: OkHttpClientSingleton
+        apiClientHelper: ApiClientHelper
     ): PhotosApiService {
         return ApiClientHelper.createPhotosApiService(
             apiClientHelper.baseUrl,
-            okHttpClientSingleton.plainOkhttpClient
+            OkHttpClientSingleton().plainOkHttpClient
         )
+    }
+
+    @Provides
+    @Singleton
+    internal fun providePhotoRemoteDataSource(
+        photosApiService: PhotosApiService
+    ): PhotoRemoteDataSource {
+        return PhotoRemoteDataSourceImpl(photosApiService)
+    }
+
+    @Provides
+    @Singleton
+    internal fun providePhotosModel(
+        photoRemoteDataSource: PhotoRemoteDataSource
+    ): PhotosModel {
+        return PhotosModelImpl(photoRemoteDataSource)
     }
 }
---
+++ app/src/main/java/com/hyejeanmoon/wallpaperbyunsplash/domain/scenes/photos/datasource/PhotoRemoteDataSource.kt
@@ -0,0 +1,38 @@
+package com.hyejeanmoon.wallpaperbyunsplash.domain.scenes.photos.datasource
+
+import com.hyejeanmoon.wallpaperbyunsplash.domain.scenes.photos.entity.Photo
+
+/**
+ * PhotoRemoteDataSource
+ *
+ * It's a photo of remote data source interface.
+ */
+interface PhotoRemoteDataSource {
+
+    /**
+     * get a photo from server by id.
+     */
+    suspend fun getPhoto(id: String): Photo
+
+    /**
+     * get a list of photos from server by parameters.
+     */
+    suspend fun getPhotos(page: Int, perPage: Int, orderBy: String): List<Photo>
+
+    /**
+     * get a list of photos from server with random by parameters.
+     */
+    suspend fun getRandomPhotos(
+        collections: String,
+        featured: String,
+        userName: String,
+        query: String,
+        orientation: String,
+        count: String
+    ): List<Photo>
+
+    /**
+     * get a photo from server with random without any parameters.
+     */
+    suspend fun getRandomPhoto(): Photo
+}
