247a9aa911e2e2e5e976910ae04997e75a34c453
HyejeanMoon <wenhaichen1992@gmail.com>
2020-03-22 17:54:12 +0900

    create DataSourceFactory by dagger2

--- app/src/main/java/com/hyejeanmoon/moonunsplash/presentation/scenes/home/photos/LatestPhotosDataSourceFactory.kt
+++ app/src/main/java/com/hyejeanmoon/moonunsplash/presentation/scenes/home/photos/LatestPhotosDataSourceFactory.kt
@@ -3,13 +3,15 @@ package com.hyejeanmoon.moonunsplash.presentation.scenes.home.photos
 import androidx.paging.DataSource
 import com.hyejeanmoon.moonunsplash.domain.scenes.photos.PhotosModel
 import com.hyejeanmoon.moonunsplash.domain.scenes.photos.entity.Photo
+import javax.inject.Singleton
 
 /**
- * PhotosDataSourceFactory
+ * LatestPhotosDataSourceFactory
  *
  * It's a Factory for PhotoDataSource that help DataSource to create.
  */
-class PhotosDataSourceFactory(private val model: PhotosModel, private val mode: String) :
+@Singleton
+class LatestPhotosDataSourceFactory(private val model: PhotosModel, private val mode: String) :
     DataSource.Factory<Int, Photo>() {
 
     override fun create(): DataSource<Int, Photo> {
